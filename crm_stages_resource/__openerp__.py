2f9dfc93b845163e29549d686db3d2abc072be53
Andrius Laukavičius <info@oerp.lt>
2014-09-25 16:55:56 +0300

    [IMP] notify_user

--- crm_stages_resource/__openerp__.py
+++ crm_stages_resource/__openerp__.py
@@ -3,7 +3,7 @@
 #    
 #    Odoo, Open Source Management Solution
 #
-#    Author: Andrius Laukavičius. Copyright: Andrius Laukavičius
+#    Author: Andrius Laukavičius. Copyright: JSC NOD Baltic
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
