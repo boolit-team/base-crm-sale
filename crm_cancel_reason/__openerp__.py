c3ce0709b1b42d58a9fa7a1fa505834af02af432
Andrius Laukavičius <info@oerp.lt>
2014-09-26 15:12:33 +0300

    [ADD] base_reason and crm_cancel_reason as specific implementation

---
+++ crm_cancel_reason/__openerp__.py
@@ -0,0 +1,55 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    
+#    Odoo, Open Source Management Solution
+#
+#    Author: Andrius Laukavičius. Copyright: JSC NOD Baltic
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+
+
+{
+    'name': 'Opportunity Cancel Reasons',
+    'version': '1.0',
+    'category': 'Reasons',
+    'sequence': 2,
+    'summary': 'Manage Cancel Reasons',
+    'description': """
+	Lets you define and choose cancel reasons for Opportunities.
+    Uses base_reason module as a base for reason management.
+	""",
+    'author': 'OERP',
+    'website': 'www.oerp.eu',
+    'depends': [
+        'base_reason', 'crm',      
+    ],
+    'data': [
+        #'security/ir.model.access.csv',
+        'views/crm_lead_view.xml',
+        #'data/,        
+
+    ],
+    'demo': [
+    ],
+    'test': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': [],
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ crm_cancel_reason/models/crm_lead.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields
+
+class reason(models.Model):
+    _inherit = 'base.reason'
+
+    opportunity_id = fields.Many2one('crm.lead', 'Opportunity')
+
+class crm_lead(models.Model):
+    _inherit = 'crm.lead'
+
+    reason_ids = fields.One2many('base.reason', 'opportunity_id', 'Reasons')
