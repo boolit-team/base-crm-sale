543b9bb2835ecc40fba24847fe67474f0fb5b3ce
Andrius Laukavičius <info@oerp.lt>
2015-03-09 13:41:47 +0200

    [FIX] changed uid source. Now it takes from context as self.env.uid was providing superuser id which is not needed here

--- smtp_per_user/smtp_per_user.py
+++ smtp_per_user/smtp_per_user.py
@@ -10,8 +10,9 @@ class ir_mail_server(models.Model):
     @api.model
     def send_email(self, message, mail_server_id=None, smtp_server=None, smtp_port=None,
                    smtp_user=None, smtp_password=None, smtp_encryption=None, smtp_debug=False):
-        mail_server = self.search([('user_id', '=', self.env.uid)], limit=1)
+        #get uid from context. self.env.uid does not always provide current uid
+        mail_server = self.search([('user_id', '=', self.env.context.get('uid'))], limit=1) 
         if mail_server:
             mail_server_id = mail_server.id
-        return super(ir_mail_server, self).send_email(message, mail_server_id, smtp_server, smtp_port,
-                   smtp_user, smtp_password, smtp_encryption, smtp_debug)
+        return super(ir_mail_server, self).send_email(message=message, mail_server_id=mail_server_id, smtp_server=smtp_server, smtp_port=smtp_port,
+                   smtp_user=smtp_user, smtp_password=smtp_password, smtp_encryption=smtp_encryption, smtp_debug=smtp_debug)
